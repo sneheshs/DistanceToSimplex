d358af48efeecee579f11a8b35949c5c5b318659
Snehesh Shrestha <snehesh@umd.edu>
2020-07-22 16:03:34 -0400

    Added simplex distance calculation of multiple points optimization. NOTE: distance value on the multiple seems inaccurate after couple of calculations -- TODO: fix this bug

--- distanceToSimplex.py
+++ distanceToSimplex.py
@@ -69,3 +69,136 @@ def distanceToSimplex(point, S):
             distance, projection = distanceToSimplex(point, np.array(S[1:]))
 
     return distance, projection
+
+global precomputed_S_values
+precomputed_S_values = {}
+
+def distanceToSimplex_single(point, S):
+    '''
+    Optimization for multiple points to a single simplex
+
+    Author: Snehesh Shrestha (https://github.com/sneheshs)
+    '''
+
+    global precomputed_S_values
+
+    n = np.shape(S)[0]  
+
+    if n == 1:
+        distance = np.linalg.norm(point-S[0])
+        projection = np.matrix(S[0]).T
+    else:
+        the_key = str(np.shape(S))
+        if not the_key in precomputed_S_values:
+            #print("Computing", the_key)
+
+            translatedS = S-S[0]        
+            A = np.matrix((translatedS[1:])).T
+            _, pivcol = frref(A) # FAST RREF
+            A = A[:, pivcol]
+            top = np.dot(A.T, A)
+            P = np.dot(A, np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)) # P = A*inv(A'*A)*A'
+
+            # Save for later
+            precomputed_S_values[the_key] = A, top, P            
+        else:
+            #print("----- Loading", the_key)
+            A, top, P = precomputed_S_values[the_key]
+
+        b = np.matrix((point-S[0])).T
+        bottom = np.dot(A.T, b)
+        alpha = np.linalg.solve(top, bottom)    #divide
+        pprime = np.dot(P, b)
+
+        posflag = 1
+
+        if any(alpha < 0):
+            posflag = 0
+
+        if sum(alpha) <= 1 and posflag == 1:
+            # projection inside simplex
+            distance = np.linalg.norm(pprime-b)
+            projection = np.matrix(pprime+S[0]).T
+        elif posflag == 0:
+            Sprime = []
+            Sprime.append(S[0]) #Sprime[0] = S[0]
+
+            for ii in range(0, np.shape(alpha)[0]):
+                if alpha[ii] > 0:
+                    Sprime.append(S[ii+1])
+                    
+            distance, projection = distanceToSimplex_single(point, np.array(Sprime))
+        else:
+            distance, projection = distanceToSimplex_single(point, np.array(S[1:]))
+
+    return distance, projection
+
+
+def distancesToSimplex(points, S):
+    '''
+    Optimization for multiple points to a single simplex
+
+    Author: Snehesh Shrestha (https://github.com/sneheshs)
+    '''
+
+    global precomputed_S_values
+    precomputed_S_values = {}
+
+    distances = []
+    projections = []
+
+    n = np.shape(S)[0]
+
+    if n == 1:
+        for point in points:
+            distances.append(np.linalg.norm(point-S[0]))
+            projections.append(np.matrix(S[0]).T)
+    else:
+        the_key = str(np.shape(S))
+        if not the_key in precomputed_S_values:
+            #print("Computing", the_key)
+
+            translatedS = S-S[0]        
+            A = np.matrix((translatedS[1:])).T
+            _, pivcol = frref(A) # FAST RREF
+            A = A[:, pivcol]
+            top = np.dot(A.T, A)
+            P = np.dot(A, np.dot(np.linalg.inv(np.dot(A.T, A)), A.T)) # P = A*inv(A'*A)*A'
+
+            # Save for later
+            precomputed_S_values[the_key] = A, top, P            
+        else:
+            #print("----- Loading", the_key)
+            A, top, P = precomputed_S_values[the_key]
+            
+        for point in points:                
+            b = np.matrix((point-S[0])).T
+            bottom = np.dot(A.T, b)
+            alpha = np.linalg.solve(top, bottom)    #divide
+            pprime = np.dot(P, b)
+
+            posflag = 1
+
+            if any(alpha < 0):
+                posflag = 0
+
+            if sum(alpha) <= 1 and posflag == 1:
+                # projection inside simplex
+                distance = np.linalg.norm(pprime-b)
+                projection = np.matrix(pprime+S[0]).T
+            elif posflag == 0:
+                Sprime = []
+                Sprime.append(S[0]) #Sprime[0] = S[0]
+
+                for ii in range(0, np.shape(alpha)[0]):
+                    if alpha[ii] > 0:
+                        Sprime.append(S[ii+1])
+                        
+                distance, projection = distanceToSimplex_single(point, np.array(Sprime))
+            else:
+                distance, projection = distanceToSimplex_single(point, np.array(S[1:]))
+
+            distances.append(distance)
+            projections.append(projection)
+
+    return distances, projections
--- distanceToSimplex_test.py
+++ distanceToSimplex_test.py
@@ -9,6 +9,27 @@ def test(svecs):
     distance, projection = distanceToSimplex(vecs[0], vecs[1:])
     return distance, projection
 
+def test_multi_onebyone(svecs):
+    vecs = np.array(svecs).astype(float)
+    distances = []
+    projections = []
+    num_of_items = 5
+    for i in range(0, num_of_items):
+        distance, projection = distanceToSimplex(vecs[i], vecs[num_of_items:])
+        distances.append(distance)
+        projections.append(projection)
+    
+    return distances, projections
+
+def test_multi(svecs):
+    vecs = np.array(svecs).astype(float)
+    points = []
+    num_of_items = 5
+    for i in range(0, num_of_items):
+        points.append(vecs[0])
+    distances, projections = distancesToSimplex(points, vecs[num_of_items:])
+    return distances, projections
+
 def load_vecs(filename):
     f = open(filename, 'r')
     lines = f.read().split('\n')
@@ -22,24 +43,70 @@ def load_vecs(filename):
 
     return svecs
 
-all_test_cases = []
+def test_case1():
+    # SANITY TEST
+    all_test_cases = []
+    all_test_cases.append([[.5,.5,.5],[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
+    all_test_cases.append(load_vecs('test_set1.csv'))
+    all_test_cases.append(load_vecs('test_set2.csv'))
 
-# SANITY TEST
-all_test_cases.append([[.5,.5,.5],[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
-all_test_cases.append(load_vecs('test_set1.csv'))
-all_test_cases.append(load_vecs('test_set2.csv'))
+    for sv in all_test_cases:
+        time_taken['rref'] = 0
+        time_taken['frref'] = 0
+        time_taken['total'] = 0
 
-for sv in all_test_cases:
-    time_taken['rref'] = 0
-    time_taken['frref'] = 0
-    time_taken['total'] = 0
+        STARTTIME = time.time()
+        distance, _ = test(sv)
+        print('Simplex Distance', distance)
+        time_taken['total'] = time.time()-STARTTIME
+        
+        # # Uncomment this to display timing values
+        print(time_taken)
+        # print('RREF % time taken', time_taken['rref']/time_taken['total'])
+        # print('FRREF % time taken', time_taken['frref']/time_taken['total'], '\n\n')
 
-    STARTTIME = time.time()
-    distance, _ = test(sv)
-    print('Simplex Distance', distance)
-    time_taken['total'] = time.time()-STARTTIME
-    
-    # Uncomment this to display timing values
-    # print(time_taken)
-    # print('RREF % time taken', time_taken['rref']/time_taken['total'])
-    # print('FRREF % time taken', time_taken['frref']/time_taken['total'], '\n\n')
+
+def test_case2():
+    time1 = []
+    time2 = []
+
+    all_test_cases = []
+    all_test_cases.append(load_vecs('test_set2.csv'))
+
+    for sv in all_test_cases:
+        time_taken['rref'] = 0
+        time_taken['frref'] = 0
+        time_taken['total'] = 0
+
+        STARTTIME = time.time()
+        distances, _ = test_multi_onebyone(sv)
+        #print('Simplex Distance One by One', distances)
+        time_taken['total'] = time.time()-STARTTIME
+        
+        time1.append(time_taken['total'])
+
+
+    for sv in all_test_cases:
+        time_taken['rref'] = 0
+        time_taken['frref'] = 0
+        time_taken['total'] = 0
+
+        STARTTIME = time.time()
+        distances, _ = test_multi(sv)
+        #print('Simplex Distance Multi', distances)
+        time_taken['total'] = time.time()-STARTTIME
+        
+        time2.append(time_taken['total'])
+
+    return np.mean(time1), np.mean(time2)
+
+
+t1 = []
+t2 = []
+for repeat in range(0, 100):
+    _t1, _t2 = test_case2()
+    t1.append(_t1)
+    t2.append(_t2)
+
+print(np.mean(t1), np.mean(t2))
+
